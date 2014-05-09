ef8dc3123931959a6dd689aa52b50e82580b9a2c
Giuseppe Mazzapica <giuseppe.mazzapica@gmail.com>
2014-05-09 19:31:50 +0200

    no need to require Hooks.php from boot.php

--- boot.php
+++ boot.php
@@ -8,8 +8,4 @@ if ( function_exists( 'add_action' ) ) {
     add_action( 'brain_init', function( $brain ) {
         $brain->addModule( new Brain\Striatum\BrainModule );
     } );
-
-    add_action( 'brain_loaded', function() {
-        require_once __DIR__ . '/Hooks.php';
-    }, 0 );
 }
