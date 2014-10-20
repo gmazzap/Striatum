df545098afe53e4cc562b3250e4d832637696718
Giuseppe Mazzapica <giuseppe.mazzapica@gmail.com>
2014-10-20 18:58:59 +0200

    Make tests compatible with Pimple 3.0

--- tests/src/FunctionalTestCase.php
+++ tests/src/FunctionalTestCase.php
@@ -7,7 +7,7 @@ class FunctionalTestCase extends TestCase {
     function setUp() {
         parent::setUp();
         \Brain\Container::flush();
-        $brain = \Brain\Container::boot( new \Pimple, FALSE, FALSE );
+        $brain = \Brain\Container::boot( new \Pimple\Container, FALSE, FALSE );
         $module = new \Brain\Striatum\BrainModule;
         $module->getBindings( $brain );
         $module->boot( $brain );
